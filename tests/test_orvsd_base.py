fe452b1bbab76ac763ef2ef3c02b06c1d04f7706
Ian Kronquist <iankronquist@teleport.com>
2014-09-22 23:27:15 -0700

    Base class with useful functions for testing orvsd

---
+++ tests/test_orvsd_base.py
@@ -0,0 +1,64 @@
+from unittest import TestCase
+
+from orvsd_central.models import User
+from orvsd_central.database import create_db_session
+
+
+class BaseORVSDTest(TestCase):
+    """
+    Functions useful for testing ORVSD.
+    """
+
+    def setUp(self):
+        self.app = setup_app()
+        self.test_client = self.app.test_client()
+
+    def login(self, username, password):
+        """
+        Login a user.
+        """
+        return self.test_client.post('/login', data=dict(
+            name=username,
+            password=password
+        ), follow_redirects=True)
+
+    def register(self, user, password, role, email):
+        """
+        Regiser a user.
+        """
+        return self.test_client.post('/register', data=dict(
+            user=user,
+            password=password,
+            confirm_pass=password,
+            role=role,
+            email=email), follow_redirects=True)
+
+    def delete_user(self, username):
+        """
+        Delete a user directly from the database.
+        """
+        with self.app.app_context():
+            session = create_db_session()
+            admin = session.query(User).filter(User.name == username).first()
+            session.delete(admin)
+            session.commit()
+
+    def add_user(self, username, email, password, role):
+        """
+        Add a user directly to the database. Role may be an integer indicating
+        the permissions of the user or may be one of the following strings:
+        * normal
+        * helper
+        * admin
+        """
+        roles = {
+            'normail': 1,
+            'helper': 2,
+            'admin': 3,
+        }
+        with self.app.app_context():
+            session = create_db_session()
+            user = User(username, email, password,
+                        role if type(role) == int else roles[role])
+            session.add(user)
+            session.commit()
